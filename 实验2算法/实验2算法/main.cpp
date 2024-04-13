25cec543210fd73ef5845e5d353e4a4eb3c68b6e
jiangbing22 <505139705@qq.com>
2024-04-13 13:41:54 +0800

    4.13

--- 实验2算法/实验2算法/main.cpp
+++ 实验2算法/实验2算法/main.cpp
@@ -1,12 +1,88 @@
 #include"to_DFA.hpp"
 #include<iostream>
 using namespace std;
+void print_NFA(NFA_graph NFA)
+{
+	GRAPH result = NFA.get_graph();
+	cout << "start:" << NFA.get_start() << " end:" << NFA.get_end() << endl;
+	int count = 0;
+	for (auto a : result)
+	{
+		cout << " node:" << count << " ";
+		for (auto b : a)
+		{
+			char key = b.first;
+			cout << "vertex: " << key << " ";
+			cout << "arrive: ";
+			for (auto c : a[key])
+			{
+				cout << c << " ";
+			}
+		}
+		count++;
+		cout << endl;
+	}
+
+}
+void print_DFA(DFA_graph DFA, NFA_graph NFA)
+{
+	DFA_GRAPH result = DFA.get_Graph();
+	cout << "state_graph:  ";
+	for (auto i : DFA.state_map)
+	{
+		for (auto j : i.first)
+		{
+			cout << j << " ";
+		}
+		cout << i.second << endl;
+	}
+
+
+
+	cout << "start:" << DFA.get_start() << " end:";
+	for (auto i : DFA.get_end())
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	int count = 0;
+	for (auto a : result)
+	{
+		cout << " node:" << count << " ";
+		cout << "numbers: ";
+		for (auto b : a.first)
+		{
+			cout << b << " ";
+		}
+		for (auto c : a.second)
+		{
+			cout << "vertex:" << c.first << " ";
+			cout << "arrive:";
+			for (auto c : a.second[c.first])
+			{
+				cout << c << " ";
+			}
+		}
+		count++;
+		cout << endl;
+	}
+
+}
+
 int main()
 {
 	string init;
-	cin >> init;
+	getline(std::cin,init);
 	RE temp(init);
-	cout << temp.get_re() << endl;
-	cout << temp.get_post();
+	cout << "regex:" << temp.get_re() << endl;
+	cout << "regex_post:" << temp.get_post() << endl;
+	NFA_graph NFA;
+	DFA_graph DFA;
+	NFA.build_NFA(init);
+	print_NFA(NFA);
+	cout << "-------------------------" << endl;
+	DFA.build_from_NFA(NFA);
+	print_DFA(DFA, NFA);
+
 
 }
--- 实验2算法/实验2算法/to_DFA.hpp
+++ 实验2算法/实验2算法/to_DFA.hpp
@@ -1,83 +1,120 @@
 #include <unordered_map>
 #include <stack>
 #include <string>
+#include<unordered_set>
+#include<iostream>
+#include<queue>
 #include<set>
+#define GRAPH std::vector<unordered_map<char, vector<int>>>
+#define DFA_GRAPH std::vector<pair<vector<int>,unordered_map<char,vector<int>>>>
 using namespace std;
-
 class RE {
 public:
     RE(string initial) : re(initial) {
         init();
-        /*to_post();*/
+        to_post();
     };
     void init();
     string get_post() { return re_post; };
     string get_re() { return re; };
 private:
     unordered_map<char, int> prior = {
-        {'*', 100}, {'+', 100}, {'?', 100}, {'.', 80}, {'|', 60}, {'@', 70}
+        {'*', 100}, {'+', 100}, {'?', 100}, {'|', 60}, {'&', 70}
+    };
+    unordered_set<char> calculator = {
+    '*', '+', '?' , '|' ,'&','(',')','\\','[',']'
     };
     string re;
     string re_post;
     void to_post();
 };
-
 void RE::init() {
     string result = "";
     string regex = re;
+    regex.erase(remove(regex.begin(), regex.end(),' '), regex.end());
     for (size_t i = 0; i < regex.length(); i++) {
 
         if (regex[i] != '[')
         {
             result += regex[i];
-        } 
-
+        }
         if (i == regex.length() - 1)
         {
             break;
         }
-        if (isalpha(regex[i])||regex[i] == '*' || regex[i] == '+' || regex[i] == '?'||regex[i]==')')
+        if (calculator.find(regex[i])==calculator.end() || regex[i] == '*' || regex[i] == '+' || regex[i] == '?' || regex[i] == ')')
         {
             
-            if (isalpha(regex[i + 1]) || regex[i + 1] == '(')
+            if (calculator.find(regex[i+1]) == calculator.end() || regex[i + 1] == '('||regex[i+1]=='['||regex[i+1]=='\\')
             {
-                result.push_back('@');
+                result.push_back('&');
             }
         }
         if (regex[i] == '[')
         {
-            i++;
             result.push_back('(');
-            while(regex[i] != ']')
-            {
-                result.push_back(regex[i]);
-                if (regex[i + 1] != ']')
-                {
-                    result.push_back('|');
+            if(regex[i+2]=='-')
+            { 
+                while (regex[i + 2] == '-'&&regex[i+1]!=']') {
+                    for (char j = regex[i + 1]; j <= regex[i + 3]; j++)
+                    {
+                        result.push_back(j);
+                        result.push_back('|');
+                        
+                    }
+                    i += 3;
                 }
+                result.erase(result.length() - 1);
+                result.push_back(')');
                 i++;
+
             }
-            result.push_back(')');
+            else
+            {
+                i++;
+                while (regex[i] != ']')
+                {
+                    result.push_back(regex[i]);
+                    if (regex[i + 1] != ']')
+                    {
+                        result.push_back('|');
+                    }
+                    i++;
+                }
+                result.push_back(')');
+            }
+        }
+        if (regex[i] == '\\')
+        {
+            result += regex[i + 1];
+            if (i + 2 < regex.length())
+            {
+                if (isalpha(regex[i + 2]) || regex[i + 2] == '('||regex[i+2]=='\\')
+                {
+                    result.push_back('&');
+                }
+                i += 1;
+                continue;
+            }
+            else
+            {
+                break;
+            }
         }
     }
     re = result;
 }
-
 void RE::to_post() {
     if (re.empty()) {
         re_post = "";
         return;
     }
-
     stack<char> operators;
     string postfix = "";
     string infix = re;
-
     for (char c : infix) {
-        if (isalnum(c)) {
-            
+        if (calculator.find(c)==calculator.end()) {
             postfix += c;
-            
         }
         else if (c == '\\')
         {
@@ -101,16 +138,12 @@ void RE::to_post() {
             operators.push(c);
         }
     }
-
     while (!operators.empty()) {
         postfix += operators.top();
         operators.pop();
     }
-
     re_post = postfix;
 }
-
-
 class NFA_graph
 {
 public:
@@ -121,7 +154,7 @@ public:
     }
     void init(char a);
     
-    size_t size() const
+    int size() const
     {
         return Graph.size();
     }
@@ -136,21 +169,38 @@ public:
             Graph.resize(n);
             return Graph[n];
         }
-   
     }
-    
+    vector<unordered_map<char, vector<int>>> get_graph()
+    {
+        return Graph;
+    }
+    int get_start() { return start; };
+    int get_end() { return end; };
+    set<char> getkey();
     NFA_graph& operator&(NFA_graph& b);
     NFA_graph& operator|(NFA_graph& b);
     NFA_graph& operator*();
     NFA_graph& operator+();
     NFA_graph& optional();
-    NFA_graph build_NFA(RE regex);
+    void build_NFA(RE regex);
 private:
     vector<unordered_map<char, vector<int>>> Graph;
     int start, end;
     void contact(NFA_graph& b);
+    unordered_set<char> calculator = {'*', '+', '?' ,'.', '|' ,'&','(',')','\\'};
 };
-
+set<char> NFA_graph::getkey()
+{
+    set<char> result;
+    for (auto i : Graph)
+    {
+        for (auto j : i)
+        {
+            result.insert(j.first);
+        }
+    }
+    return result;
+}
 void NFA_graph::contact(NFA_graph& b)
 {
     int offset = size();
@@ -170,7 +220,6 @@ NFA_graph& NFA_graph::operator&(NFA_graph& b)
     contact(b);
     this->Graph[this->end]['$'].push_back(b.start);
     this->end = b.end;
-
     return *this;
 }
 NFA_graph& NFA_graph::operator|(NFA_graph& b)
@@ -197,14 +246,15 @@ NFA_graph& NFA_graph::operator*()
     Graph[this->end]['$'].push_back(this->start);
     int old_start = this->start;
     int old_end = this->end;
-
+    //新开始节点
     unordered_map<char, vector<int>> newstart;
     Graph.push_back(newstart);
     this->start = Graph.size() - 1;
+    //新结束节点
     unordered_map<char, vector<int>> newend;
     Graph.push_back(newend);
     this->end = Graph.size() - 1;
-
+    //创建闭包连接
     Graph[this->start]['$'].push_back(this->end);
     Graph[this->start]['$'].push_back(old_start);
     Graph[old_end]['$'].push_back(this->end);
@@ -213,16 +263,22 @@ NFA_graph& NFA_graph::operator*()
 } 
 NFA_graph& NFA_graph::operator+()
 {
-    NFA_graph temp= *this;
-    temp = temp & (*temp);
-    return temp;
+    //复制本体，防止递归调用
+    NFA_graph temp1 = *this;
+    NFA_graph temp2 = *this;
+    NFA_graph result = temp1 & *temp2;
+    //结果赋值回本体
+    this->Graph = result.get_graph();
+    this->start = result.get_start();
+    this->end = result.get_end();
+    
+    return *this;
 }
 NFA_graph& NFA_graph::optional()
 {
     Graph[this->start]['$'].push_back(this->end);
     return *this;
 }
-
 void NFA_graph::init(char a)
 {
     Graph.resize(2);
@@ -230,86 +286,310 @@ void NFA_graph::init(char a)
     this->start = 0;
     Graph[0][a] = { this->end };
 }
-NFA_graph NFA_graph::build_NFA(RE re)
+void NFA_graph::build_NFA(RE re)
 {
     stack<NFA_graph> stk;
     string regex = re.get_post();
-    for (auto c : regex)
+    for (auto c=regex.begin();c!=regex.end();c++)
     {
-        if (isalpha(c))
+        if (calculator.find(*c)==calculator.end())
         {
             NFA_graph temp;
-            temp.init(c);
+            temp.init(*c);
+            stk.emplace(temp);
+        }
+        else if (*c == '\\')
+        {
+            c++;
+            NFA_graph temp;
+            temp.init(*c);
             stk.emplace(temp);
         }
         else
         {
-            switch (c)
+            switch (*c)
             {
-            case '&':
-            {
-                NFA_graph graph1 = stk.top();
-                stk.pop();
-                NFA_graph graph2 = stk.top();
-                stk.pop();
-                NFA_graph result = graph1 & graph2;
-                stk.push(result);
-                break;
-            }
+                case '&':
+                {
+                    NFA_graph graph1 = stk.top();
+                    stk.pop();
+                    NFA_graph graph2 = stk.top();
+                    stk.pop();
+                    NFA_graph result = graph2 & graph1;
+                    stk.push(result);
+                    break;
+                }
 
-            case '|':
-            {
-                NFA_graph graph1 = stk.top();
-                stk.pop();
-                NFA_graph graph2 = stk.top();
-                stk.pop();
-                NFA_graph result = graph1 | graph2;
-                stk.push(result);
-                break;
-            }
-            case '*':
-            {
-                NFA_graph graph1 = stk.top();
-                stk.pop();
-                NFA_graph result = *graph1;
-                stk.push(result);
-                break;
-            }
-            case '+':
-            {
-                NFA_graph graph1 = stk.top();
-                stk.pop();
-                NFA_graph result = +graph1;
-                stk.push(result);
-                break;
-            }
-            case '?':
-            {
-                NFA_graph graph1 = stk.top();
-                stk.pop();
-                NFA_graph result = graph1.optional();
-                stk.push(result);
-                break;
-            }
+                case '|':
+                {        
+                    if (stk.size() == 1)
+                    {
+                        NFA_graph graph1 = stk.top();
+                        stk.pop();
+                        NFA_graph result = graph1.optional();
+                        stk.push(result);
+                    }
+                    else
+                    {
+                        NFA_graph graph1 = stk.top();
+                        stk.pop();
+                        NFA_graph graph2 = stk.top();
+                        stk.pop();
+                        NFA_graph result = graph2 | graph1;
+                        stk.push(result);
+                    }
+                    break;
+                }
+                case '*':
+                {
+                    NFA_graph graph1 = stk.top();
+                    stk.pop();
+                    NFA_graph result = *graph1;
+                    stk.push(result);
+                    break;
+                }
+                case '+':
+                {
+                    NFA_graph graph1 = stk.top();
+                    stk.pop();
+                    NFA_graph result = +graph1;
+                    stk.push(result);
+                    break;
+                }
+                case '?':
+                {
+                    NFA_graph graph1 = stk.top();
+                    stk.pop();
+                    NFA_graph result = graph1.optional();
+                    stk.push(result);
+                    break;
+                }
             }
                 
         }
     }
-    NFA_graph result = stk.top();
-    return result;
+    this->Graph = stk.top().get_graph();
+    this->start = stk.top().start;
+    this->end = stk.top().end;
+    return;
 }
-
-
-class DFA
+class DFA_graph
 {
 public:
+    void build_from_NFA(NFA_graph NFA);
+    void minimize();
+    int get_start() { return start; };
+    set<int> get_end() { return end; };
+    DFA_GRAPH get_Graph() { return Graph; };
+    void init(char a); 
+    size_t size() const
+    {
+        return Graph.size();
+    }
 
-
+    unordered_map<string, int> state_map;//判断集合是否已经存在
+    unordered_set<char> get_key() { return DFA_key_set; };
 private:
-   
+    int start;
+    set<int> end;
+    DFA_GRAPH Graph;
+    vector<unordered_map<char,int>> minimizeGraph;
+    unordered_set<char> DFA_key_set;
+    string vectorToString(vector<int>& t)
+    {
+        string res = "{";
+        for (auto p : t)
+        {
+            res += to_string(p) + ",";
+        }
+        res += "}";
+        return res;
+    }
+    string setToString(set<int>& t)
+    {
+        string res = "{";
+        for (auto p : t)
+        {
+            res += to_string(p) + ",";
+        }
+        res += "}";
+        return res;
+    }
 
-
-
+    vector<int> stringToIntVector(string s)
+    {
+        vector<int> res;
+        int temp = 0;
+        for (int i = 1; i < s.size() - 1; i++)
+        {
+            if (isdigit(s[i]))
+            {
+                temp = temp * 10 + (s[i] - '0');
+            }
+            else
+            {
+                res.push_back(temp);
+                temp = 0;
+            }
+        }
+        return res;
+    }
+    string mapToString(const unordered_map<char, int>& mp) {
+        string res;
+        for (const auto& p : mp) {
+            res += p.first;
+            res += to_string(p.second);
+        }
+        return res;
+    }
+    pair<vector<int>,unordered_map<char, vector<int>>>& operator[](int n)
+    {
+        if (n < Graph.size())
+        {
+            return Graph[n];
+        }
+        else
+        {
+            Graph.resize(2*n);
+            return Graph[n];
+        }
+    }
 };
+void DFA_graph::build_from_NFA(NFA_graph NFA)
+{   
+
+    GRAPH nfa = NFA.get_graph(); //NFA图的存储结构
+    queue<vector<int>> state_queue;//状态遍历辅助队列
+    set<char> key_set = NFA.getkey();//所有边的可能取值
+
+    auto BFS_Closure = [&](const vector<int>& states) //BFS找到起始条件的epsilon闭包
+     {
+            set<int> closure;
+            unordered_map<int, bool> visited;//访问标签
+            for (int state : states) {
+                closure.insert(state);
+            }
+            queue<int> q;//遍历辅助队列
+            for (int state : states) {
+                q.push(state);
+                visited[state] = true;
+                while (!q.empty()) {
+                    int current = q.front();
+                    q.pop();
+                    for (int next : nfa[current]['$']) {
+                        if (!visited[next]) {
+                            closure.insert(next);
+                            q.push(next);
+                            visited[next] = true;
+                        }
+                    }
+                }
+            }
+            return closure;
+    };
+    set<int> startstate = BFS_Closure({ NFA.get_start() });
+    state_queue.push(vector<int>(startstate.begin(),startstate.end()));
+    //state_map[setToString(startstate)]=this->Graph.size();
+    while (!state_queue.empty())
+    {
+        vector<int> current_state = state_queue.front();
+        state_queue.pop();
+        if (current_state.empty())
+        {
+            continue;
+        }
+        for (auto c : key_set)
+        {
+            if (c == '$')
+            {
+                continue;
+            }
+            this->DFA_key_set.insert(c);
+            set<int> next_state;
+            for (auto i : current_state)
+            {
+                for (auto j : nfa[i][c])
+                {
+                    if (nfa[i].find(c) != nfa[i].end()) 
+                    {
+                        next_state.insert(j);
+                    }
+                }
+            }
+            set<int> next_state_closure = BFS_Closure(vector<int>{next_state.begin(), next_state.end()});//找到下一个状态中所有闭包
+            vector<int> temp(next_state_closure.begin(), next_state_closure.end());//将set转换为vector
+            string state_key = setToString(next_state_closure);
+            if (state_map.find(vectorToString(current_state))!= state_map.end())
+            {
+                this->Graph[Graph.size() - 1].second[c] = temp;
+            }
+            else
+            {
+                state_map[vectorToString(current_state)] = Graph.size();
+                this->Graph.push_back({ current_state,{} });
+                this->Graph[Graph.size() - 1].second[c] = temp;
+            }
+            if (state_map.find(state_key)==state_map.end())
+            {
+                state_queue.push(temp);
+            }
+        }
+    }
+    this->start = 0;
+    for (auto& entry : state_map) {
+        string key = entry.first;
+        if (key.find(to_string(NFA.get_end())) != string::npos) {
+           this->end.insert(entry.second);
+        }
+    }
+    return;
+};
+void DFA_graph::minimize()
+{
+    //找出集合划分
+    set<int> end_set_index = this->end;
+    set<int> else_set_index;//非终态集合
+    for (auto i : state_map)//初始化终态集合
+    {
+        if (this->end.find(i.second) != this->end.end())
+        {
+            else_set_index.insert(i.second);
+        }
+    }
+    //遍历DFA，将状态集合转换为划分;
+    vector<unordered_map<char, int>> partition_graph;//划分图，true表示在终态集合中,false表示不在，-1表示空;
+    for (auto state : this->Graph)
+    {
+        auto t_map = state.second;//复制一份
+        unordered_map<char, int> result;//将要插入划分图的元素
+        for (auto key : DFA_key_set)
+        {
+            if (t_map[key].empty())
+            {
+                result[key] = -1;
+            }
+            else if (end_set_index.find(state_map[vectorToString(t_map[key])]) != end_set_index.end())//在终态集合中
+            {
+                result[key] = true;
+            }
+            else
+            {
+                result[key] = false;
+            }
+        }
+        partition_graph.push_back(result);
+    }
+    unordered_map<string, bool>p_map;
+    for (auto map : partition_graph)
+    {
+        string map_key = mapToString(map);
+        if (!p_map[map_key])
+        {
+            this->minimizeGraph.push_back(map);
+            p_map[map_key] = true;
+        }
+    }
+    return;
+}
 
 
